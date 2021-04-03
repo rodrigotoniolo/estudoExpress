734b6638a1ee9c1b6ce40ff9128fa03b72ab40cc
Rodrigo Toniolo <rodrigotoniolo@hotmail.com>
2021-04-03 17:44:22 -0300

    finalizando express

--- index.js
+++ index.js
@@ -1,55 +1,62 @@
-const { request, response } = require('express');
-const express = require('express'); //criar modulo
+const express = require('express');
 const {v4: uuidv4} = require('uuid');
+
 const app = express();
 
 app.use(express.json());
 
-
 const projects = [];
 
-app.get('/projects', (request,response) => {
-    //const {title, owner} = request.query;
+app.get('/projects', (request, response) => {
+    //const { title, owner } = request.query;
 
-    return response.json(projects);
-        
+    return response.json(projects)
 });
 
 app.post('/projects', (request, response) => {
-    const {title, owner} = request.body
+    const { title, owner } = request.body;
 
     const project = { id: uuidv4(), title, owner}
-    projects.push(project); // esse push vai jogar a criação do nosso projeto para o nosso array.
 
-    return response.json(project); // sempre retornar o projeto recém criado e nunca exibir a lista completa.
-})
+    projects.push(project); // esse push vai jogar a criação do nosso projeto para o nosso array
 
-app.put('/projects/:id', (request, response) =>{
-    const params = request.params;
+    return response.json(project);  // sempre retornar o projeto recén criado e nunca exibir a lista completa
+});
 
-    console.log(params);
-    
-    return response.json([
-        'Projeto 50',
-        'Projeto 2',
-        'Projeto 3',
-        'Projeto 4',
-    ])
-})
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params; // aqui pegamos nosso ID
+    const { title, owner } = request.body; // retornando uma nova informação
 
-app.delete('/projects/:id', (request, response) =>{
-    return response.json([
-        'Projeto 50',
-        'Projeto 2',
-    ])
-})
+    const projectIndex = projects.findIndex(project => project.id === id);
 
-app.listen(3000, () => {
-    console.log('Servidor rodando!');
-})
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Projeto não foi encontrado' });
+    }
 
-/**
- * Query Params: Vamos usar principalmente para filtros e paginação
- * Route Params: Identificar recursos na hora de atualizar ou deletar.
- * Request Body: Resto do conteúdo na hora de criar ou editar um recurso.
- */
+    const project = {
+        id,
+        title,
+        owner,
+    }
+
+    projects[projectIndex] = project;
+
+
+    return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Projeto não foi encontrado' });
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send();
+});
+
+app.listen(3333);
